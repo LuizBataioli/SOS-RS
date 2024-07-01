528336ded58f6c1be0e2add9c250ac841bf1b21f
Luiz Bataioli <136658924+LuizBataioli@users.noreply.github.com>
2024-06-30 22:31:10 -0300

    sosRS

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// script.js
+document.getElementById('sintomas-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const sintomasInput = document.getElementById('sintomas').value;
+    const sintomas = sintomasInput.split(',').map(sintoma => sintoma.trim().toLowerCase());
+
+    const doencas = {
+        'leptospirose': ['febre Alta', 'dor de cabeça', 'sangramento', 'dor muscular', 'calafrio', 'olhos vermelhos', 'vomito'],
+        'tétano': ['contrações musculares na mandibula', 'contrações musculares no pescoço', 'dificuldade para respirar', 'febre', 'pressão alta', 'sudorese'],
+        'diarreia aguda': ['sangramento nas fezes', 'dores abdominais', 'muco nas fezes'],
+        'hepatite A': ['fadiga', 'pele amarela', 'urina escura', 'fezes claras', 'perda de apetite'],
+        'dengue': ['febra alta', 'dor muscular', 'manchas vermelhas no corpo', 'dores nas articulações', 'hemorragia', 'dificuldade para respirar', 'inchaço', 'dor de cabeça'],
+        'animais peçonhentos (aranhas, cobra, lacraias e escorpiões)': ['Dor' ,'eritema', 'hematoma', 'formação de bolhas']
+    };
+
+    let maiorCoincidencia = 0;
+    let doencaProvavel = null;
+
+    for (const [doenca, sintomasDoenca] of Object.entries(doencas)) {
+        const coincidencias = sintomasDoenca.filter(sintoma => sintomas.includes(sintoma)).length;
+        if (coincidencias > maiorCoincidencia) {
+            maiorCoincidencia = coincidencias;
+            doencaProvavel = doenca;
+        }
+    }
+
+    let resultado = doencaProvavel
+        ? `Os sintomas mais prováveis correspondem à doença: ${doencaProvavel}`
+        : 'Não foi possível identificar a doença com os sintomas fornecidos.';
+
+    document.getElementById('resultado').textContent = resultado;
+});
