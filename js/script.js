eeb80514dc1eed1ae6c5b114637b0698a81428e1
Luiz Bataioli <136658924+LuizBataioli@users.noreply.github.com>
2024-07-02 19:42:34 -0300

    SOSRS

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ document.getElementById('sintomas-form').addEventListener('submit', function(eve
         'diarreia aguda': ['sangramento nas fezes', 'dores abdominais', 'muco nas fezes'],
         'hepatite A': ['fadiga', 'pele amarela', 'urina escura', 'fezes claras', 'perda de apetite'],
         'dengue': ['febra alta', 'dor muscular', 'manchas vermelhas no corpo', 'dores nas articulações', 'hemorragia', 'dificuldade para respirar', 'inchaço', 'dor de cabeça'],
-        'animais peçonhentos (aranhas, cobra, lacraias e escorpiões)': ['Dor' ,'eritema', 'hematoma', 'formação de bolhas']
+        'animais peçonhentos (aranhas, cobra, lacraias e escorpiões)': ['dor' ,'eritema', 'hematoma', 'formação de bolhas']
     };
 
     let maiorCoincidencia = 0;
